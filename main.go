7d5ea645337b524fcfa7c4549b451c37094dc9f2
thirumurug <123375144+thirumurug@users.noreply.github.com>
2023-02-21 15:15:50 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:Thiru#2001@tcp(localhost:3306)/emp")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println("Successfully Connected Golang with Mysql")
+}
